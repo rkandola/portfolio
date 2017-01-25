187e3731a1f122098deab9f760b40a30cf00b422
rkandola <rkandola@one.verizon.com>
2017-01-25 10:09:12 -0500

    add config

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+module.exports = {
+	'connection' : {
+		'host' : 'localhost',
+		'user' : 'nodeuser',
+		'password' : 'password'
+	},
+	'database' : 'portfolio',
+	'users_table' : 'users',
+	'portfolio_table' : 'position'
+};
