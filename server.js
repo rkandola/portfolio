7566067b8b1784868ef93d9688165278ab9ca94c
rkandola <rkandola@one.verizon.com>
2017-01-27 08:09:47 -0500

    refactor api to from backend to api directory

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
     var express  = require('express');
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 
-    var app = require('./backend/app');
+    var app = require('./api/app');
     app.set('port', process.env.PORT || 4000);
     app.use('/', express.static(__dirname + '/public'));
     app.listen(4000);
